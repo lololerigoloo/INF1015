8c6959feb822733619a0927fecdc55a697d59916
lololerigoloo <laurentcorbeil05@gmail.com>
2026-02-13 11:58:30 -0500

    petit modif tp3 base sams les fichier td2

--- TP3/build/CMakeFiles/ProjetTD3_couverture.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ProjetTD3_couverture"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ProjetTD3_couverture.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- TP3/build/CMakeFiles/ProjetTD3_couverture.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ProjetTD3_couverture.
-# This may be replaced when dependencies are built.
