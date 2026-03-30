6fbd5baf9e3e1ce55aa42faab861a42a903813dc
lololerigoloo <laurentcorbeil05@gmail.com>
2026-03-30 19:31:33 -0400

    fini le tp5 partie deux avec l'utilisation d'une map

---
+++ TD5_2457246_2460464/lectureBinaire.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <fstream>
+#include <cstdint>
+using namespace std;
+// Entêtes copiées du main.cpp fourni (ou du fichier du TD3):
+
+using UInt8  = uint8_t;
+using UInt16 = uint16_t;
+
+UInt8 lireUint8(istream& fichier);
+UInt16 lireUint16(istream& fichier);
+string lireString(istream& fichier);
